2ed224721898c796cf079799fd59af3ea0f75ad8
Patrick Ware <patrick.r.ware@gmail.com>
2020-05-19 20:46:21 -0700

    apiData fetching data from api

--- js/main.js
+++ js/main.js
@@ -2,22 +2,26 @@ function render() {
   console.log("render function invoked...")
 }
 
+let apiData = [];
 function doFetch() {
-  console.log("getting data from API...")
+  console.log("getting data from API...");
   
-//  value = document.querySelector("#search").value;
-  api = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson';
+  //value = document.querySelector("#search").value;
+  api = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2020-01-01&endtime=2020-05-07&minmagnitude=5&minlatitude=24.396308&minlongitude=-124.848974&maxlatitude=49.384358&maxlongitude=-66.885444";
   fetch(api)
     .then(response => response.json())
     .then(data => {
         console.log("fetch successful");
-        console.log("this is data", data.data);
-        apiGifData = data.data;
+        console.log("this is data", data);
+        apiData = data.features;
     });
+}
 
+function loopData() {
   // Looping through an object called "apiData"
-//  for (const [key, value] of Object.entries(apiData)) {
-//      console.log('this is key', key);
-//      console.log('this is value', value);
-//}
+  // console.log("data is", apiData)
+  for (const [key, value] of Object.entries(apiData)) {
+      console.log('this is key', key);
+      console.log('this is value', value);
+  }
 } 
