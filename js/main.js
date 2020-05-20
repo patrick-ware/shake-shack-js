4cb4c17f02697d37b3f048a872bb00e2aaf42017
Patrick Ware <patrick.r.ware@gmail.com>
2020-05-19 22:47:42 -0700

    CSS margin updated to be % for BarChar-bar

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ function doFetch() {
   console.log("getting data from API...");
   
   //value = document.querySelector("#search").value;
-  api = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2020-01-01&endtime=2020-05-19&minmagnitude=5&minlatitude=24.396308&minlongitude=-124.848974&maxlatitude=49.384358&maxlongitude=-66.885444";
+  api = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2018-01-01&endtime=2020-05-19&minmagnitude=5&minlatitude=24.396308&minlongitude=-124.848974&maxlatitude=49.384358&maxlongitude=-66.885444";
   fetch(api)
     .then(response => response.json())
     .then(data => {
@@ -28,14 +28,16 @@ function loopData() {
 
 function addBars() {
   let chartDiv = document.querySelector(".BarChart");
-
+  console.log("--------adding data to chart")
   // Clear anything that might be in the div
   chartDiv.innerHTML = '';
   for (const [key, value] of Object.entries(apiData)) {
+    let mag = value.properties.mag
     let newBar = document.createElement('div');
-    let barHeight = value.properties.mag;    
+    let barHeight = mag*10;
+    newBar.textContent = mag.toFixed(1);   
     newBar.className = "BarChart-bar";
-    newBar.style.height = barHeight*10 +'%';
+    newBar.style.height = barHeight +'%';
     
     chartDiv.appendChild(newBar);
   }
