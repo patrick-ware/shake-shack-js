997a70ffcd127c5b832214500a5cd5cf8f7f23ea
Patrick Ware <patrick.r.ware@gmail.com>
2020-05-20 22:45:57 -0700

    addBars() changed to be render(), added to doFetch() but currently asynchronous

--- js/main.js
+++ js/main.js
@@ -1,32 +1,4 @@
 function render() {
-  console.log("render function invoked...")
-}
-
-let apiData = [];
-function doFetch() {
-  console.log("getting data from API...");
-  
-  //value = document.querySelector("#search").value;
-  api = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2018-01-01&endtime=2020-05-19&minmagnitude=5&minlatitude=24.396308&minlongitude=-124.848974&maxlatitude=49.384358&maxlongitude=-66.885444";
-  fetch(api)
-    .then(response => response.json())
-    .then(data => {
-        console.log("fetch successful");
-        console.log("this is data", data);
-        apiData = data.features;
-    });
-}
-
-function loopData() {
-  // Looping through an object called "apiData"
-  // console.log("data is", apiData)
-  for (const [key, value] of Object.entries(apiData)) {
-      console.log('this is mag', value.properties.mag);
-      console.log('this is place', value.properties.place);
-  }
-} 
-
-function addBars() {
   let chartDiv = document.querySelector(".BarChart");
   console.log("--------adding data to chart")
   // Clear anything that might be in the div
@@ -42,3 +14,30 @@ function addBars() {
     chartDiv.appendChild(newBar);
   }
 }
+
+let apiData = [];
+function doFetch() {
+  console.log("getting data from API...");
+  
+  //value = document.querySelector("#search").value;
+  api = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2020-01-01&endtime=2020-05-19&minmagnitude=5&minlatitude=24.396308&minlongitude=-124.848974&maxlatitude=49.384358&maxlongitude=-66.885444";
+  fetch(api)
+    .then(response => response.json())
+    .then(data => {
+        console.log("fetch successful");
+        console.log("this is data", data);
+        apiData = data.features;
+    });
+
+  console.log("now render please");
+  render();
+}
+
+function loopData() {
+  // Looping through an object called "apiData"
+  // console.log("data is", apiData)
+  for (const [key, value] of Object.entries(apiData)) {
+      console.log('this is mag', value.properties.mag);
+      console.log('this is place', value.properties.place);
+  }
+} 
