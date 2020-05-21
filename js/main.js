ff16ddc1b2244f9ac0b1e49b98549f92f75fea52
Patrick Ware <patrick.r.ware@gmail.com>
2020-05-20 23:04:15 -0700

    doFetch() changed to an async function with await so that API data comes before rendering

--- js/main.js
+++ js/main.js
@@ -16,12 +16,12 @@ function render() {
 }
 
 let apiData = [];
-function doFetch() {
+async function doFetch() {
   console.log("getting data from API...");
   
   //value = document.querySelector("#search").value;
   api = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2020-01-01&endtime=2020-05-19&minmagnitude=5&minlatitude=24.396308&minlongitude=-124.848974&maxlatitude=49.384358&maxlongitude=-66.885444";
-  fetch(api)
+  await fetch(api)
     .then(response => response.json())
     .then(data => {
         console.log("fetch successful");
